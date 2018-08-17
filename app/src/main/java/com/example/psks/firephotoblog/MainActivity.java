396ea9877ac3095c30e2d521b9cc1cc511fcd9c8
PsKs <pongsakorn.psks@gmail.com>
2018-08-18 06:53:18 +0700

    Create user logout

--- app/src/main/java/com/example/psks/firephotoblog/MainActivity.java
+++ app/src/main/java/com/example/psks/firephotoblog/MainActivity.java
@@ -3,16 +3,34 @@ package com.example.psks.firephotoblog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mainToolbar;
+
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // [START initialize_auth]
+        mAuth = FirebaseAuth.getInstance();
+        // [END initialize_auth]
+
+        mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolbar);
+
+        getSupportActionBar().setTitle("FirePhotoBlog ( ﾟдﾟ)");
     }
 
     @Override
@@ -24,11 +42,38 @@ public class MainActivity extends AppCompatActivity {
             // User is signed in
         } else {
             // No user is signed in
-            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-            startActivity(loginIntent);
-            finish();
+            sendToSignIn();
         }
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
 
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_logout_btn:
+
+                signOut();
+
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private void sendToSignIn() {
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+        finish();
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+        sendToSignIn();
     }
 }
