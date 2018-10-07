baccf519f3db781a1e25a641d8ff08185e8becb9
PsKs <pongsakorn.psks@gmail.com>
2018-10-08 05:02:32 +0700

    Added BlogRecyclerAdapter to Home fragment.

--- app/src/main/java/com/example/psks/firephotoblog/HomeFragment.java
+++ app/src/main/java/com/example/psks/firephotoblog/HomeFragment.java
@@ -3,9 +3,27 @@ package com.example.psks.firephotoblog;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import io.opencensus.tags.Tag;
 
 
 /**
@@ -13,6 +31,12 @@ import android.view.ViewGroup;
  */
 public class HomeFragment extends Fragment {
 
+    private RecyclerView blog_list_view;
+    private List<BlogPost> blog_list;
+    private BlogRecyclerAdapter blogRecyclerAdapter;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -22,8 +46,46 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        blog_list = new ArrayList<>();
+        blog_list_view = view.findViewById(R.id.blog_post_view);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        blogRecyclerAdapter = new BlogRecyclerAdapter(blog_list);
+        blog_list_view.setLayoutManager(new LinearLayoutManager(container.getContext()));
+        blog_list_view.setAdapter(blogRecyclerAdapter);
+
+        if (firebaseAuth.getCurrentUser() != null) {
+
+            firebaseFirestore = FirebaseFirestore.getInstance();
+
+            Query firstQuery = firebaseFirestore.collection("posts").orderBy("timestamp", Query.Direction.DESCENDING);
+
+            firstQuery.addSnapshotListener(new EventListener<QuerySnapshot>() {
+                @Override
+                public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                    if (queryDocumentSnapshots != null) {
+
+                        for (DocumentChange doc : queryDocumentSnapshots.getDocumentChanges()) {
+
+                            if (doc.getType() == DocumentChange.Type.ADDED) {
+
+                                BlogPost blogPost = doc.getDocument().toObject(BlogPost.class);
+                                blog_list.add(blogPost);
+
+                                blogRecyclerAdapter.notifyDataSetChanged();
+                            }
+                        }
+                    }
+                }
+            });
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
     }
 
 }
